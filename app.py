78f3b9af0dd8ff77e51ca763877d67c991e86849
Rigat <47829410+rigsboss@users.noreply.github.com>
2023-04-06 23:16:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LinearRegression
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score
+#from sklearn.linear_model import LinearRegression
+#from sklearn.neighbors import KNeighborsRegressor
+#from sklearn.model_selection import train_test_split
+#from sklearn.metrics import mean_squared_error, r2_score
 df = pd.read_csv('Data\student-mat.csv')
 #Title for the app
 st.title('Secondary School Student Performance Prediction')
